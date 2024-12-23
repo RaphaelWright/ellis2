15dbaa90f82d1d4f06d4517a2d8670e0441eb521
Raphael Wright Agbedanu <51016286+RaphaelWright@users.noreply.github.com>
2024-12-23 13:44:09 +0000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        wildLove: ["Wild Love"],
+        Atma: ["Atma"],
+        AtmaBold: ["Atma-Bold"],
+        Itim: ["Itim-Regular"],
+      },
+    },
+  },
+  plugins: [],
+};
